5cec0410bd0ccb531a0421bba1b0368370d5305a
yhc <46378093+yhc1338@users.noreply.github.com>
2021-01-30 17:20:46 +0800

    Update plotball.m

--- plotball.m
+++ plotball.m
@@ -1,11 +1,12 @@
 function plotball(ball)
+global border
 l=length(ball);
 for i=1:l
     r=ball(i).r; theta=0:pi/100:2*pi;
     x=r*cos(theta)+ball(i).s(1); 
     y=r*sin(theta)+ball(i).s(2);
     plot(x,y,'-')
-    axis([-10 40 -10 40])
+    axis(border)
     hold on
 end
 for k=1:l
